53596fd89b768fe01bf124ad3936f6c316829f08
Yaiito <jonathan.perez@ynov.com>
2024-09-26 21:38:34 +0200

    Create rules.go

---
+++ rules.go
@@ -0,0 +1,12 @@
+package gogame
+
+import "fmt"
+
+func DisplayRules() {
+    fmt.Println("=== Règles du Jeu ===")
+    fmt.Println("1. Le but est de deviner un mot lettre par lettre.")
+    fmt.Println("2. Vous avez 10 essais pour deviner le mot.")
+    fmt.Println("3. Chaque mauvaise lettre réduit le nombre d'essais.")
+    fmt.Println("4. Si vous devinez le mot entier et que c'est incorrect, vous perdez 2 essais.")
+    fmt.Println("Bonne chance!")
+}
