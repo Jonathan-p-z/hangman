49492c0be6cb911f742e4da5afa12fe6e9031514
Yaiito <jonathan.perez@ynov.com>
2024-09-27 08:32:02 +0200

    Update rules.go

--- rules.go
+++ rules.go
@@ -1,12 +1,17 @@
-package gogame
+package game
 
 import "fmt"
 
 func DisplayRules() {
-    fmt.Println("=== Règles du Jeu ===")
-    fmt.Println("1. Le but est de deviner un mot lettre par lettre.")
-    fmt.Println("2. Vous avez 10 essais pour deviner le mot.")
-    fmt.Println("3. Chaque mauvaise lettre réduit le nombre d'essais.")
-    fmt.Println("4. Si vous devinez le mot entier et que c'est incorrect, vous perdez 2 essais.")
-    fmt.Println("Bonne chance!")
+	clearScreen()
+	fmt.Println("=== Règles du Jeu ===")
+	fmt.Println("1. Le but est de deviner un mot lettre par lettre.")
+	fmt.Println("2. Vous avez 10 essais pour deviner le mot.")
+	fmt.Println("3. Chaque mauvaise lettre réduit le nombre d'essais.")
+	fmt.Println("4. Si vous devinez le mot entier et que c'est incorrect, vous perdez 2 essais.")
+	fmt.Println("Bonne chance!")
+	fmt.Println("Appuyez sur 'Entrée' pour revenir au menu principal...")
+
+	var input string
+	fmt.Scanln(&input)
 }
