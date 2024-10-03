d4bd1a58dd6a1baa716030a962c85ad55df18816
Yaiito <jonathan.perez@ynov.com>
2024-10-03 09:39:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"gogame/game" // Assurez-vous que cela correspond à votre structure de projet
+	"gogame/game"
+	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	game.DisplayMenu() // Appel de la fonction pour afficher le menu
+	game.DisplayMenu()
+}
+
+func displayWordWithFirstLetterRevealed(word string, lettersGuessed map[string]bool) {
+	firstLetter := string(word[0])
+	lettersGuessed[firstLetter] = true
+	for _, letter := range word {
+		letterStr := string(letter)
+		if lettersGuessed[letterStr] {
+			fmt.Printf("%s ", letterStr)
+		} else {
+			fmt.Print("_ ")
+		}
+	}
+	fmt.Println()
+}
+
+func reader() {
+	file, err := os.Open("Hangman\\test\\mots_diffciles.txt")
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du fichier:", err)
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	fmt.Println("Contenu du fichier:")
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier:", err)
+	}
 }
